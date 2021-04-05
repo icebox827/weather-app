07d175cab3214d80dcf4520503380c8735215595
Denis Lafontant <icebox827@gmail.com>
2021-04-05 17:35:50 -0400

    Update api.js and create pageload.js

--- src/api.js
+++ src/api.js
@@ -4,11 +4,32 @@ const api = (() => {
 
   const getData = async (city) => {
     const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`, { mode: 'cors' });
-
-    const jsonObj = await response.json();
-
+    
     try {
-      data = 
+      const jsonObj = await response.json();
+
+      data.name = jsonObj.name;
+      data.country = jsonObj.sys.country;
+      data.temp = jsonObj.main.temp
+      data.weather = jsonObj.weather[0].descrption;
+      data.icon = jsonObj.weather[0].icon;
+      data.maxTemp = jsonObj.main.temp_max;
+      data.minTem = jsonObj.main.temp_min;
+      data.pressure = jsonObj.main.pressure;
+      data.humidity = jsonObj.main.humidity;
+      data.visibility = jsonObj.visibility;
+      data.windSpeed = jsonObj.wind.speed;
+      data.winDeg = jsonObj.wind.deg;
+      data.cloud = jsonObj.cloud.all;
+      data.sunRise = jsonObj.sys.sunrise;
+      data.sunSet = jsonObj.sys.sunset;
+    } catch (error) {
+      if(error.cod === '404') {
+        data.cod = jsonObj.cod;
+      }
     }
-  }
+  };
+  return {getData, data};
 })();
+
+export default api;
