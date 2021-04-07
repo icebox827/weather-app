895ca0519bba50010252ce75bb49223aa9614f70
Denis Lafontant <icebox827@gmail.com>
2021-04-07 12:45:03 -0400

    Remove console.log in api.js

--- src/api.js
+++ src/api.js
@@ -2,13 +2,7 @@ const api = async (city) => {
   const key = '537fc8e3a71f25b7c88777911464f3c4';
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
   const response = await fetch(url, { mode: 'cors' }).then(e => e.json()); 
-  console.log(response);
-  console.log(response.name);
-  console.log(response.sys.country);
-  console.log(response.weather[0].description);
-  console.log(response.main.temp);
-  console.log(response.main.humidity);
-  console.log(response.main.pressure);
+  
   return response
 };
 
