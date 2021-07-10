afcfb9e4a75f7f3b52aa684f133b0ca2866d6331
Denis Lafontant <icebox827@gmail.com>
2021-07-10 14:34:41 -0400

    Update API call

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const api = async (city) => {
   const key = '537fc8e3a71f25b7c88777911464f3c4';
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
   const response = await fetch(url, { mode: 'cors' }).then((e) => e.json());
 
   return response;
