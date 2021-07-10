271363ec3231d7effa02ed69eb1d2f6122b564a9
Denis Lafontant <icebox827@gmail.com>
2021-07-10 16:59:39 -0400

    Create main DOM element

--- src/pageload.js
+++ src/pageload.js
@@ -14,6 +14,88 @@ function header() {
   return header;
 }
 
+function main() {
+  const main = document.createElement('main');
+  const form = document.createElement('form');
+  const cityLabel = document.createElement('label');
+  const cityInput = document.createElement('input');
+  const searchBtn = document.createElement('button');
+  const card = document.createElement('card');
+  const cardTitle = document.createElement('div');
+  const cardSubtitle = document.createElement('div');
+  const cardText = document.createElement('div');
+  const city = document.createElement('h3');
+  const country = document.createElement('h4');
+  const weather = document.createElement('h5');
+  const temperatureLabel = document.createElement('h5');
+  const temperature = document.createElement('h5');
+  const minTemperatureLabel = document.createElement('h5');
+  const minTemperature = document.createElement('h5');
+  const maxTemperatureLabel = document.createElement('h5');
+  const maxTemperature = document.createElement('h5');
+  const pressureLabel = document.createElement('h5');
+  const pressure = document.createElement('h5');
+  const humidityLabel = document.createElement('h5');
+  const humidity = document.createElement('h5');
+
+  main.classList.add('main');
+  main.setAttribute('id', 'main');
+
+  form.setAttribute('id', 'myForm');
+
+  cityLabel.classList.add('form-label');
+  cityLabel.setAttribute('for', 'city');
+
+  cityInput.classList.add('form-input');
+  cityInput.setAttribute('type', 'text');
+  cityInput.setAttribute('name', 'city');
+  cityInput.setAttribute('id', 'cityInput');
+  cityInput.setAttribute('placeholder', 'Enter location');
+
+  searchBtn.classList.add('submit', 'btn-primary');
+  searchBtn.setAttribute('type', 'submit');
+  searchBtn.setAttribute('value', 'Search');
+  searchBtn.setAttribute('id', 'button');
+
+  card.classList.add('card');
+  cardTitle.classList.add('card-title');
+  cardSubtitle.classList.add('card-subtitle');
+  cardText.classList.add('card-text');
+  temperatureLabel.classList.add('temp');
+  minTemperatureLabel.classList.add('minTemp');
+  maxTemperatureLabel.classList.add('maxTemp');
+  pressureLabel.classList.add('pres');
+  humidityLabel.classList.add('hum');
+
+  city.setAttribute('id', 'city');
+  country.setAttribute('id', 'country');
+  weather.setAttribute('id', 'weather');
+  temperature.setAttribute('id', 'temperatute');
+  minTemperature.setAttribute('id', 'minTemperature');
+  maxTemperature.setAttribute('id', 'maxTemperature');
+  pressure.setAttribute('id', 'pressure');
+  humidity.setAttribute('id', 'humidity');
+
+  cityLabel.innerHTML = '<h2>Enter city</h2>';
+  cityInput.innerHTML = '<i class="fa fa-search"></i>';
+  searchBtn.innerHTML = 'Search location';
+  temperatureLabel.innerHTML = 'Temperature in Celsius<i class="fas fa-thermometer-half"></i>';
+  minTemperatureLabel.innerHTML = 'Temperature in Celsius<i class="fas fa-thermometer-half"></i>';
+  maxTemperatureLabel.innerHTML = 'Temperature in Celsius<i class="fas fa-thermometer-half"></i>';
+  pressureLabel.innerHTML = 'Pressure hPa<i class="fas fa-tachometer-alt"></i>';
+  humidityLabel.innerHTML = 'Humidity %<i class="fas fa-tint"></i>';
+
+  form.appendChild(cityLabel);
+  form.appendChild(cityInput);
+  card.appendChild(cardTitle);
+  card.appendChild(cardSubtitle);
+  card.appendChild(cardText);
+  main.appendChild(form);
+  main.appendChild(card);
+
+  return main;
+}
+
 function footer() {
   const footer = document.createElement('footer');
   const copyright = document.createElement('h5');
@@ -40,65 +122,11 @@ async function loadPage() {
   const fetchdata = await api('Quebec');
 
   content.appendChild(header());
+  content.appendChild(main());
   content.appendChild(footer());
 }
 
 export default loadPage;
-
-// function main() {
-//   const main = document.createElement('main');
-//   main.classList.add('main');
-//   main.setAttribute('id', 'main');
-
-//   const form = document.createElement('form');
-//   form.setAttribute('id', 'myForm');
-
-//   const cityLabel = document.createElement('label');
-//   const cityInput = document.createElement('input');
-//   const searchBtn = document.createElement('button');
-
-//   cityLabel.setAttribute('for', 'City');
-//   cityLabel.classList.add('form-label');
-//   cityInput.setAttribute('type', 'text');
-//   cityInput.setAttribute('id', 'cityInput');
-//   cityInput.setAttribute('name', 'City');
-//   cityInput.setAttribute('placeholder', 'Enter the name of a city.');
-
-//   searchBtn.setAttribute('id', 'button');
-//   searchBtn.setAttribute('type', 'submit');
-//   searchBtn.setAttribute('value', 'Search');
-//   searchBtn.classList.add('submit', 'btn-primary');
-
-//   cityLabel.innerHTML = '<h2>City name</h2>';
-//   cityInput.innerHTML = '<i class="fa fa-search" aria-hidden="true"></i>';
-//   searchBtn.innerHTML = 'Search location';
-
-//   const card = document.createElement('card');
-//   const cardCity = document.createElement('h3');
-//   const cardCountry = document.createElement('h4');
-//   const cardWeather = document.createElement('h5');
-//   const cardTempLabel = document.createElement('h5');
-//   const cardTemp = document.createElement('h5');
-//   const cardHumLabel = document.createElement('h5');
-//   const cardHumidity = document.createElement('h5');
-//   const cardPresLabel = document.createElement('h5');
-//   const cardPressure = document.createElement('h5');
-
-//   card.classList.add('card');
-//   cardTempLabel.classList.add('temp');
-//   cardHumLabel.classList.add('hum');
-//   cardPresLabel.classList.add('pres');
-
-//   cardTempLabel.innerHTML = 'Temperature K<i class="fas fa-thermometer-half"></i>';
-//   cardHumLabel.innerHTML = 'Humidity %<i class="fas fa-tint"></i>';
-//   cardPresLabel.innerHTML = 'Pressure hPa<i class="fas fa-tachometer-alt"></i>';
-
-//   cardCity.setAttribute('id', 'city');
-//   cardCountry.setAttribute('id', 'country');
-//   cardWeather.setAttribute('id', 'weather');
-//   cardTemp.setAttribute('id', 'temp');
-//   cardHumidity.setAttribute('id', 'humidity');
-//   cardPressure.setAttribute('id', 'pressure');
 
 //   card.appendChild(cardCity);
 //   card.appendChild(cardCountry);
